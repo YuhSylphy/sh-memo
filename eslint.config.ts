6c4000d45f7ff6c36fae7fd4bc357cd8a7a634b5
YuhSylphy <yuhsylphy@gmail.com>
2026-05-20 12:01:43 +0900

    Configure linter

--- eslint.config.ts
+++ eslint.config.ts
@@ -21,6 +21,11 @@ export default defineConfig([
 				rules: {
 					semi: ['error', 'always'],
 					'react/react-in-jsx-scope': 'off',
+					'no-unused-vars': 'off',
+					'@typescript-eslint/no-unused-vars': [
+						'warn',
+						{ argsIgnorePattern: '^_' },
+					],
 				},
 			},
 		],
